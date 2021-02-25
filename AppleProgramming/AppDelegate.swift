f6a956f706bef19025f8f2502dc90a15b9819396
Tsungyu Yu <ytyubox@gmail.com>
2021-02-25 16:31:22 +0800

    project init

---
+++ AppleProgramming/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+/* 
+ *		Created by 游宗諭 in 2021/2/25
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 10.15
+ */
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate { }
+
---
+++ AppleProgramming/ViewController.swift
@@ -0,0 +1,17 @@
+//
+/* 
+ *		Created by 游宗諭 in 2021/2/25
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 10.15
+ */
+
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+
+}
+
